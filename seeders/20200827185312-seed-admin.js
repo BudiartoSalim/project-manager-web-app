e2a8939dddd66e6595c5b5e48e95def8396a2f75
mstr.monank@gmail.com <mstr.monank@gmail.com>
2020-08-28 02:02:00 +0700

    seed admin

---
+++ seeders/20200827185312-seed-admin.js
@@ -0,0 +1,30 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+   let data = [{
+     admin_username: 'admin01',
+     password: '12345'
+   }]
+   return queryInterface.bulkInsert('Admins',data,{});
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    return queryInterface.bulkDelete('Admins',null,{});
+  }
+};
